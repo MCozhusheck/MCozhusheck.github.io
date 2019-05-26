ee5300c7e7fa99e45b29cd0928bdf1df355f150f
MCozhusheck <mackozuszek@gmail.com>
2019-05-26 20:09:19 +0200

    Basic parser

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import React from 'react';
 import './App.css';
 var Tone = require('tone');
 var ohm = require('ohm-js');
-const file = require('./parser/musicLang.ohm');
+
+let musicLang = `musicLang {
+  triggerAttackRelease = "TriggerAttackRelease" " " ident " " ident
+
+  ident = alnum+
+}`
 
 function App() {
-
-  let gramma = ohm.grammar('musicLang { triggerAttackRelease = "TriggerAttackRelease" " " alnum+ " " alnum+}');
+  
+  const synth = new Tone.MembraneSynth().toMaster();
+  let gramma = ohm.grammar(musicLang);
   let input = 'TriggerAttackRelease C4 8n';
+  let semnantics = gramma.createSemantics().addOperation('eval', {
+    triggerAttackRelease: function(first, second, third, fourth, fiveth) {
+      return synth.triggerAttackRelease(third.eval(), fiveth.eval())
+    },
+    ident: function(e) {
+      console.log(this.sourceString)
+      return this.sourceString
+    }
+  })
+
   let m = gramma.match(input);
   if(m.succeeded()) {
     console.log('matched')
   } else {
     console.log('did not match')
   }
-  const synth = new Tone.MembraneSynth().toMaster();
+  semnantics(m).eval()
+
 
   function playMusic() {
-    synth.triggerAttackRelease('C4', '8n')
+    //synth.triggerAttackRelease('C4', '8n')
+    semnantics(m).eval()
   }
   
   return (
